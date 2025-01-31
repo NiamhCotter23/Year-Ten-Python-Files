7ae13765d3dbcf1d49aa5344b42cb1b68f8bfcac
NiamhCotter(WHSG) <21-niamh.cotter@whsg.info>
2025-01-31 11:42:46 +0000

    dice

--- diceGame.py
+++ diceGame.py
@@ -1,15 +1,7 @@
+#Dice function
 import random
 def dice():
-    roll =random.randint(1,6)
-    #print(roll)
-    return roll
-    dice()
-def dice1():
-    roll1 =random.randint(1,6)
-    #print(roll1)
-    return roll1
-    dice1()
-
+    return random.randint(1,6)
 
 player1=0
 player2=0
@@ -17,29 +9,28 @@ player2=0
 for i in range(5):
     dice1=dice()
     dice2=dice()
-    print("Player1 rolled a "+ str(dice1)+ "and a "+str(dice2))
+    print(f"Player1 rolled a {dice1} and a {dice2}")
     score=dice1+dice2
 
     if score % 2 == 0:
-        score=score+10
+        score+=10
     else:
-        score=score-5
+        score-=5
         if score<0:
             score=0
-        else:
-            score=score
-player1=score
-print("Player1's total score for this round is "+str(score))
+    player1+=score
+print(f"Player1's total score for this round is {player1}")
 
 for i in range(5):
-    score1=dice1()+dice1()
+    dice3 = dice()
+    dice4 = dice()
+    print(f"Player2 rolled a {dice3} and a {dice4}")
+    score1 = dice3 + dice4
     if score1 % 2 == 0:
-        score1=score1+10
+        score1+=10
     else:
-        score1=score1-5
+        score1-=5
         if score1<0:
             score1=0
-        else:
-            score1=score1
-player1=score1
-print(player2)
+    player2+=score1
+print(f"Player2's total score for this round is {player2}")
