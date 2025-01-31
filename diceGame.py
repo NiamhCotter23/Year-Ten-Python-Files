c5b880e9fda936882bc2a01c7229bdd99a1aec2a
NiamhCotter(WHSG) <21-niamh.cotter@whsg.info>
2025-01-31 12:07:57 +0000

    dice

--- diceGame.py
+++ diceGame.py
@@ -1,3 +1,13 @@
+#rules
+print("""Rules:
+The points rolled on each player’s dice are added to their score.
+If the total is an even number, and additional 10 points are added to their score.
+If the total is an odd number, 5 points are subtracted from their score.
+The score of a player cannot go below 0 at any point.
+The person with the highest score at the end of the 5 rounds wins.
+If both players have the same score at the end of the 5 rounds, they each roll 1 die and whoever gets the highest score wins (this repeats until someone wins).
+""")
+
 #Dice function
 import random
 def dice():
@@ -6,6 +16,7 @@ def dice():
 player1=0
 player2=0
 
+#Player 1's turn
 for i in range(5):
     dice1=dice()
     dice2=dice()
@@ -21,6 +32,7 @@ for i in range(5):
     player1+=score
 print(f"Player1's total score for this round is {player1}")
 
+#Player 2's turn
 for i in range(5):
     dice3 = dice()
     dice4 = dice()
@@ -34,3 +46,17 @@ for i in range(5):
             score1=0
     player2+=score1
 print(f"Player2's total score for this round is {player2}")
+
+#Winner
+print("And now for the winner!")
+if player1>player2:
+    winner="Player1"
+    playerWin=player1
+elif player1==player2
+    winner=""
+else:
+    winner="Player2"
+    playerWin=player2
+print(f"The winner is {winner} with a score of {playerWin}")
+
+#Play again?
